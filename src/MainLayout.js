c6b2eb6cc3d95340a0f99996ab4f557f6bb8da1d
Saikat-too <singhasaikat37@gmail.com>
2024-06-19 13:44:09 +0600

    MainLayoutPage

---
+++ src/MainLayout.js
@@ -0,0 +1,16 @@
+// Mainlayout Page 
+import React from 'react';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+
+const MainLayout = ({ children }) => {
+  return (
+    <div className="main-layout">
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
