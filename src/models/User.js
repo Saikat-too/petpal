065f0aadc044c761aa9eb379b0486b3a0fd8da80
Saikat-too <saikatsingha00q@gmail.com>
2024-11-08 12:03:42 +0600

    database creation for user

---
+++ src/models/User.js
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true ,
+        trim: true,
+        min : 3 , 
+        max : 20 ,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        min : 10 , 
+        max : 50 ,
+      },
+      password: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(v) {
+              return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(v);
+            },
+            message: props => `${props.value} is not a strong enough password.`
+          },
+        min : 8 ,
+      },
+      phone: {
+        type: String,
+        required: true,
+      },
+      role: {
+        type: String,
+        enum: ['client', 'doctor'],
+        default: 'client', 
+      },
+      address: {
+        street: String,
+        city: String,
+        state: String,
+        postalCode: String,
+      },
+      pets: [
+        {
+          type: String,
+          breed: String,
+          age: Number,
+          name: String,
+        },
+      ],
+      createdAT : {
+        timestamps : true
+      },
+});
+
+const User  = mongoose.model("User" , UserSchema);
+
+export default User ;
