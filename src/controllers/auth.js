3f5624a1f889dc9e0a2d0f670bcd11359e186f2f
Saikat-too <saikatsingha00q@gmail.com>
2024-11-08 12:35:20 +0600

    middleware for picture upload in index.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,3 +1,39 @@
-import bcrypt from "brycpt";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User"
+
+/* Registration User */ 
+
+export const register = async(req , res) => {
+    try {
+        const {
+            name , 
+            email , 
+            password ,
+            phone , 
+            role ,
+            address ,
+            pets
+        } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password , salt);
+
+
+        const newUser = new User ({
+            name , 
+            email , 
+            password : passwordHash ,
+            phone , 
+            role ,
+            address ,
+            pets 
+
+        });
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+
+    } catch(err){
+        res.status(500).json({error: err.message });
+    }
+}
