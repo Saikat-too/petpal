4617ecd6363b58304f019753597e1047e19cd852
Saikat-too <saikatsingha00q@gmail.com>
2024-11-08 11:39:47 +0600

    setup the server

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import express from "express"
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/*Configurations */
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy());
+app.use(morgan("common"));
+app.use(bodyParser.json({limit: "30mb" , extended : true}));
+app.use(bodyParser.urlencoded({limit : "30mb" , extended : true}));
+app.use(cors());
+app.use("/assets" , express.static(path.join(__dirname, 'public/assets')));
+
+
+/* File Storage */ 
+const storage = multer.diskStorage({
+  destination : function (req , file , cb){
+    cb(null , "public/assets")
+  },
+  filename : function (req , file , cb){
+    cb(null , file.originalname);
+  }
+});
+
+const upload = multer({storage})
+
+/* MONGOOSE SETUP */ 
+
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  app.listen(PORT , () => console.log(`Server Port : ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`)) ; 
