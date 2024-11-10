4459d53b33f052f4b7ab80a9b436c7b54f5e2c99
Saikat-too <saikatsingha00q@gmail.com>
2024-11-10 12:10:10 +0600

    ActivityLog for login and registration

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js"
-
+import ActivityLog from "../models/Activity_log.js";
 /* Registration User */ 
 
 export const register = async(req , res) => {
@@ -32,6 +32,18 @@ export const register = async(req , res) => {
         });
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
+        
+        const userId = new savedUser._id;
+        
+     const activitylog = new ActivityLog({
+        userId ,
+        timestamp : new Date() ,
+        activityType : "Create User",
+        description : 'User created with email : ${email}'
+  
+      });
+  
+      await activitylog.save();
 
     } catch(err){
         res.status(500).json({error: err.message });
@@ -52,6 +64,19 @@ export const login = async(req , res) => {
         const token = jwt.sign({id : user._id}.process.env.JWT_SECRET);
         delete user.password;
         res.status(200).json({token , user});
+
+
+        const userId = new user._id;
+        
+        const activitylog = new ActivityLog({
+            userId ,
+            timestamp : new Date() ,
+            activityType : "LogIn",
+            description : 'User created with email : ${email}'
+    
+        });
+  
+      await activitylog.save();
         
     } catch (err) {
         
